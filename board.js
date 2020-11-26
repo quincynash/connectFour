2e11bc58f8ab801a36e69ffb71fe9bc0a7863d82
quincynash <73974411+quincynash@users.noreply.github.com>
2020-11-25 21:32:54 -0800

    Update board.js

--- board.js
+++ board.js
@@ -4,7 +4,7 @@ function drawBoard() {
       board[x][y].draw()
     }
   }
-  
+
   noFill()
   stroke(0)
   rect(0, 0, BW, BH)
@@ -12,7 +12,7 @@ function drawBoard() {
 
 function onBoard(x, y) {
   return x >= 0 && y >= 0 && x < COLS && y < ROWS
-} 
+}
 
 function piece(turn) {
   if (turn == PLAYER) {
@@ -29,7 +29,7 @@ function switchTurn() {
   }
 }
 
-function mousePos(cell=true) {
+function mousePos(cell = true) {
   var pos = createVector(mouseX - xoff, mouseY - yoff)
   if (cell) {
     pos.x = floor(pos.x / SIZE)
@@ -38,21 +38,21 @@ function mousePos(cell=true) {
   return pos
 }
 
-function drawText() { 
-  image(title, width/2-title.width/2, 0)
-  
+function drawText() {
+  image(title, width / 2 - title.width / 2, 0)
+
   fill(0)
   stroke(0)
   textAlign(CENTER, CENTER)
   textFont('Futura')
   textSize(50)
-  text("Score: " + str(playerScore) + " - " + str(computerScore), width/2, (height + yoff + BH)/2)
+  text("Score: " + str(playerScore) + " - " + str(computerScore), width / 2, (height + yoff + BH) / 2)
 }
 
 function startDrop(col, row) {
   dropping = true
   orig = createVector(col, row)
-  pos = createVector((col + 0.5) * SIZE, SIZE/2)
+  pos = createVector((col + 0.5) * SIZE, -SIZE / 2)
   vel = 0
   end = createVector((col + 0.5) * SIZE, (row + 0.5) * SIZE)
   bounces = 0
@@ -60,22 +60,22 @@ function startDrop(col, row) {
 
 function animateDrop() {
   fill(piece(turn))
-    stroke(0)
-    circle(pos.x, pos.y, SIZE - SPACE)
-    
-    vel += 0.25
-    pos.y += vel
-    
-    if (pos.y > end.y) {
-      pos.y = end.y
-      vel *= -0.3
-      bounces += 1
-    }
-    
-    if (bounces > 3) {
-      dropping = false
-      placePiece(orig.x)
-    }
+  stroke(0)
+  circle(pos.x, pos.y, SIZE - SPACE)
+
+  vel += 0.25
+  pos.y += vel
+
+  if (pos.y > end.y) {
+    pos.y = end.y
+    vel *= -0.3
+    bounces += 1
+  }
+
+  if (bounces > 3) {
+    dropping = false
+    placePiece(orig.x)
+  }
 }
 
 function emptyRow(col) {
@@ -87,7 +87,7 @@ function emptyRow(col) {
   return -1
 }
 
-function placePiece(col, swap=true) {
+function placePiece(col, swap = true) {
   board[col][emptyRow(col)].piece = piece(turn)
   if (swap) {
     switchTurn()
@@ -95,6 +95,6 @@ function placePiece(col, swap=true) {
 }
 
 function calculateOffset() {
-  xoff = width/2 - BW/2
-  yoff = height/2 - BH/2
+  xoff = width / 2 - BW / 2
+  yoff = height / 2 - BH / 2
 }
